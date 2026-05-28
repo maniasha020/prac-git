d278c7237adfedfb76283181dec50b9683eb6081
maniasha <svyatowa.m@gmail.com>
2026-05-28 13:18:15 +0300

    Added input parameters for two integers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+
+
 int main() {
+
     std::cout << "vse!" << std::endl;
+
+
+
+    int num1, num2;
+
+    std::cout << "Введите первый: ";
+
+    std::cin >> num1 >> num2;
+
+
+
     return 0;
+
 }
